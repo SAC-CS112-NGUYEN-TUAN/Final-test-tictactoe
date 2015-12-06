86091f47c181104fe29e7ff1693ef5d2c9fb33c9
SAC-CS112-NGUYEN-TUAN <tmacnguyen0525@gmail.com>
2015-12-06 03:13:47 -0800

    early working player vs player .

--- TicTacToe/src/Main.java
+++ TicTacToe/src/Main.java
@@ -1,30 +1,42 @@
 import java.util.Scanner;
 public class Main {
-	static int row, clu;
-	static Scanner input = new Scanner(System.in);
-	static char [] [] board = new char [3][3];
-	static char turn = 'x';
+	public static int ro, cl;
+	public static Scanner input = new Scanner(System.in);
+	public static char[][] board = new char[3][3];
+	public static char turn = 'X';
 	
 	public static void main(String[] args) {
+		startboardvalue();
+		play();
+	}
+	
+	public static void startboardvalue(){
 		for (int i = 0 ; i < 3; i++) {
 			for (int j = 0 ; j < 3;j++){
 				board [i][j] = '_';
 			}
 		}
-		Playboard();
-		play();
 	}
-	
 	public static void play(){
 		boolean playing = true;
+		
 		while (playing) {
-			row = input.nextInt() -1;
-			clu = input.nextInt() -1;
-			board[row][clu] = turn;
+			System.out.println("Input row    :");
+			ro = input.nextInt() - 1;
+			System.out.println("Input column :");
+			cl = input.nextInt() - 1;
 			
-			if (turn == 'x')
-				turn = 'o';
-				else turn = 'x';	
+			board[ro][cl] = turn;
+			System.out.println(board [ro][cl]); // debugg
+			
+			if (GameOver(ro,cl)){
+				playing = false;
+				System.out.println("Player "+ turn +" win!");
+			}
+			Playboard();
+			if (turn == 'X')
+				turn = 'O';
+			else turn = 'X';	
 		}
 	}
 	
@@ -34,15 +46,22 @@ public class Main {
 			System.out.println();
 			for (int j = 0 ; j < 3; j++) {
 				if (j == 0 )
-					System.out.print ("|");
-				System.out.print(board[1][j] + "|" ) ;
+					System.out.print ("| ");
+				System.out.print(board[i][j] + " | " ) ;
 			}
 		}
 		System.out.println();
 	}
 	
-	public boolean GameOver ( int rowmove, int clumove){
-		
+	public static boolean GameOver ( int rowmove, int clumove){
+		if (board[0][clumove]==board[1][clumove]&& board [0][clumove]==board [2][clumove])
+			return true;
+		if (board[rowmove][0]==board[rowmove][1]&& board [rowmove][0]==board [rowmove][2])
+			return true;
+		if (board[0][0] == board [1][1] && board[0][0]== board [2][2]&&board[1][1]!='_')
+			return true;
+		if (board[0][2] == board [1][1] && board[0][2]== board [2][0]&&board[1][1]!='_')
+			return true;
 		return false;
 	}
 }
